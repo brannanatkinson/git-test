9c02c47e629031a0547460ed3f3643dc79e3a071
Brannan Atkinson <brannanatkinson@gmail.com>
2017-02-11 18:05:49 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var sassdoc = require('sassdoc');
+
+// ... variables
+var input = './stylesheets/**/*.scss';
+var output = './public/css';
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+var sassdocOptions = {
+  dest: './public/sassdoc'
+};
+
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sassdoc())
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output))
+    // Release the pressure back and trigger flowing mode (drain)
+    // See: http://sassdoc.com/gulp/#drain-event
+    .resume();
+});
+
+gulp.task('sassdoc', function () {
+  return gulp.src(input)
+    .pipe(sassdoc());
+});
+
+gulp.task('default', ['sass', 'watch', 'sassdoc' /*, possible other tasks... */]);
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
